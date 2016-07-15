230662035a3f7fc4cfd550500b5c59f692e28973
karl longman <karl_longman@hotmail.com>
2016-07-15 12:12:29 +0100

    update

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-##Udacity Front End Developer NanoDegree
-##Project 4:
-##mobile portfolio - Website Performance Optimization
+# Udacity Front End Developer NanoDegree
+## Project 4:
+## mobile portfolio - Website Performance Optimization
 
-####What's needed before you Install?
+#### What's needed before you Install?
 
-###npm (Node.js)
+### npm (Node.js)
 your local machine must node.js installed to allow the npm package manager to run:
 
 to check if you already have it installed open a terminal and type:
@@ -15,14 +15,14 @@ to check if you already have it installed open a terminal and type:
 
  If you get an error instead,  you will have to install Node.js on to the local machine;
 
-* #####[npm homepage](https://www.npmjs.com/)
+* ##### [npm homepage](https://www.npmjs.com/)
 
-* #####[Download a Node.js installer](https://nodejs.org/en/download/)
+* ##### [Download a Node.js installer](https://nodejs.org/en/download/)
 
 
-##Install
+## Install
 
-####clone the git repository to your local machine;
+#### clone the git repository to your local machine;
 
 open a terminal where you want to clone to on your local machine
 then type:
@@ -33,7 +33,7 @@ navigate into the 'frontend-nanodegree-mobile-portfolio' directory
 
 `cd frontend-nanodegree-mobile-portfolio`
 
-####Installing the required npm packages
+#### Installing the required npm packages
 
 the easiest way to add the npm packages is to type in the terminal:
 
@@ -60,11 +60,11 @@ Then run the general `npm install` command to add the npm packages needed ;
 
 Sorry, I haven't tested this build process on a windows pc. (Please let me know if it works, so I can update this README file)
 
-####After `npm install`  stage completes sucessfully
+#### After `npm install`  stage completes sucessfully
 
 you will see a new direcory in the repo called `node_modules`
 
-##Serve up the optimized `index.html` page
+## Serve up the optimized `index.html` page
 
 type:
 
@@ -84,8 +84,7 @@ http://localhost:8080/
 
 ![localhost](https://cloud.githubusercontent.com/assets/131895/12380487/0079dec8-bd6c-11e5-84f5-bc5b84b88b9f.png)
 
-
-##Part 1: Optimize the Google PageSpeed Insights score for index.html
+## Part 1: Optimize the Google PageSpeed Insights score for index.html
 
 1. Open a browser and visit localhost:8080
 1. Download and install [ngrok](https://ngrok.com/) to make your local server accessible remotely.
@@ -101,7 +100,7 @@ use google chrome to view the `forwarding address`
 
 You can use this for input at the ;
 
-####[google page speed insights website](https://developers.google.com/speed/pagespeed/insights/)
+#### [google page speed insights website](https://developers.google.com/speed/pagespeed/insights/)
 
 An example result from my ngrok address.
 
@@ -109,7 +108,7 @@ Mobile Speed: ****97****, User experience: 100          |  Desktop speed: ****98
 :-------------------------:|:-------------------------:
 ![ngrok_mobile](https://cloud.githubusercontent.com/assets/131895/12380670/68335d56-bd70-11e5-9eb4-963b109683ba.png)  |  ![ngrok_desktop](https://cloud.githubusercontent.com/assets/131895/12380669/681dc2b6-bd70-11e5-849c-4e82493b5996.png)
 
-##`index.html` optimisations :
+## `index.html` optimisations :
 
 * created an 'offline' gulp task `imagesForSite` to create resized and optimized images in an `asset` directory within the `src` directory. This directory is copied over to the `dist` directory using the gulp task - `build`.
 * minified & inlined externaly linked but critical fonts, CSS and javascript files. (see the `build-html` gulp task in `gulpfile.js`)
@@ -118,20 +117,20 @@ Mobile Speed: ****97****, User experience: 100          |  Desktop speed: ****98
 * capitalizing the random-pizza names by using a `"pizzaName"` class CSS rule instead of javascript in `main.js`.
 
 
-##Part 2: Optimize Frames per Second in `views/pizza.html` & `views/js/main.js`
+## Part 2: Optimize Frames per Second in `views/pizza.html` & `views/js/main.js`
 
 To optimize `views/pizza.html`, you will need to modify `views/js/main.js` until your frames per second rate is 60 fps or higher.
 
-##`pizza.html` optimisations :
+## `pizza.html` optimisations :
 * used 'offline' gulp task `imagesForSite` to build resized and optimized image assets (compressed `.jpg` and `.png` images with alpha).
 * minified `pizza.html` (removed comments & whitespace), CSS and javascript files using gulp task `build`.
 
-##`main.js` optimisations :
+## `main.js` optimisations :
 most of the optimizations needed to improve the frame per seconds of the `pizza.html` page are within the javascript of `main.js`. See comments in `src/views/js/main.js` for more examples.
 
 * optimized expensive javascript operations and DOM selection calls inside for-loops within functions, to help increase the speed of both the initial `randomPizzas` and `mover` elements being added to the DOM and the  (low frames per second performance) 'janky' resizing and animation.
 * reduced the number of 'mover' class objects created from 200 to a smaller amount (approx 30-50) based on the browser window size.
 * used style `transformX()` to animate the `mover` class objects as it is a composite-only operation and is GPU accelerated on some browsers. Added a CSS `will-change: transform` rule to the `mover` class for further look-ahead type browser optimisations ;
 
-##Also done as part of the project:
+## Also done as part of the project:
 * used `npm` package manager and `Gulp` task runner to help automate some of the development tasks, such as, the building of optimized assets and a `dist` site, using using `ngrok` and `gulp` tasks for pagespeed insight rating, using `browser-sync` and `watch` to create a more dynamic feedback within the browser, help validate the html and CSS before submitting the project
--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ var gulp = require('gulp');
 
 // Include gulp plugins
 var plugins = require("gulp-load-plugins")({
-    pattern: ['gulp-*', 'gulp.*'],
-    replaceString: /\bgulp[\-.]/
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
 });
 
 // require npm modules
 var browserSync = require('browser-sync'),
-    del = require('del'),
-    runSequence = require('run-sequence'),
-    jshintStylish = require('jshint-stylish'),
-    htmlStylish = require('htmlhint-stylish'),
-    chalk = require('chalk'),
-    through2 = require('through2'),
-    gulpIf = require('gulp-if');
+  del = require('del'),
+  runSequence = require('run-sequence'),
+  jshintStylish = require('jshint-stylish'),
+  htmlStylish = require('htmlhint-stylish'),
+  chalk = require('chalk'),
+  through2 = require('through2'),
+  gulpIf = require('gulp-if');
 
 /* ////////////////////////////////////////////////////////////////////
 
@@ -27,381 +27,381 @@ after running gulp task on local machine
 ///////////////////////////////////////////////////////////////////////*/
 
 var psi = require('psi'),
-    site = 'http://a0a03eb7.ngrok.io/',
-    key = '';
+  site = 'http://a0a03eb7.ngrok.io/',
+  key = '';
 
 ////////////////////////////////////////////////////////////////////////
 
 var paths = {
-    scripts: ['src/js/*.js', 'src/views/js/*.js'],
-    styles: ['src/css/*.css', 'src/views/css/*.css'],
-    images: 'src/img/**/*',
-    content: ['src/*.html', 'src/views/*.html'],
-    assets: 'src/assets/**/*',
-    base: 'src'
-}
+  scripts: ['src/js/*.js', 'src/views/js/*.js'],
+  styles: ['src/css/*.css', 'src/views/css/*.css'],
+  images: 'src/img/**/*',
+  content: ['src/*.html', 'src/views/*.html'],
+  assets: 'src/assets/**/*',
+  base: 'src'
+};
 
 var dist = {
-    scripts: 'dist',
-    styles: 'dist',
-    assets: 'dist',
-    content: 'dist'
-}
+  scripts: 'dist',
+  styles: 'dist',
+  assets: 'dist',
+  content: 'dist'
+};
 
 ///////////////////////////////////////////////////
 // define gulp tasks
 
 function customPlumber() {
-    return plugins.plumber({
-        errorHandler: function (err) {
-            console.log(err.stack);
-            this.emit('end');
-        }
-    });
+  return plugins.plumber({
+    errorHandler: function(err) {
+      console.log(err.stack);
+      this.emit('end');
+    }
+  });
 }
 
 // serve dev version of website locally task
-gulp.task('browserSync:dev', function () {
-    browserSync({
-        server: {
-            baseDir: 'src'
-        },
-        host: 'localhost',
-        port: 8080,
-        open: false,
-        notify: false
-    })
+gulp.task('browserSync:dev', function() {
+  browserSync({
+    server: {
+      baseDir: 'src'
+    },
+    host: 'localhost',
+    port: 8080,
+    open: false,
+    notify: false
+  });
 });
 
 // serve dist optimized version of website locally task
-gulp.task('browserSync:dist', function () {
-    browserSync({
-        server: {
-            baseDir: 'dist'
-        },
-        host: 'localhost',
-        port: 8080,
-        open: false,
-        notify: false
-    })
+gulp.task('browserSync:dist', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+    host: 'localhost',
+    port: 8080,
+    open: false,
+    notify: false
+  });
 });
 
-gulp.task('imagesConfig', function () {
+gulp.task('imagesConfig', function() {
 
-    // Make configuration from existing HTML files
-    var config = plugins.responsiveConfig('./src/views/images/*.*', {
-        base: paths.base
-    });
+  // Make configuration from existing HTML files
+  var config = plugins.responsiveConfig('./src/views/images/*.*', {
+    base: paths.base
+  });
 
-    //console.log(config);
+  //console.log(config);
 
-    return gulp.src('src/views/images/*.*', {
-            base: paths.base
-        })
-        // Use config as the responsive configuration
-        .pipe(plugins.responsive(config, {
-            // Global configuration for all images
-            // The output quality for JPEG, WebP and TIFF output formats
-            quality: 70,
-            // Use progressive (interlace) scan for JPEG and PNG output
-            progressive: true,
-            // Strip all metadata
-            withMetadata: false,
-            // don't create enlarged output images if images are already less than the required dimensions.
-            errorOnEnlargement: false,
-            skipOnEnlargement: true,
-            errorOnUnusedConfig: false
-        }))
-        .pipe(gulp.dest(dist.images));
+  return gulp.src('src/views/images/*.*', {
+      base: paths.base
+    })
+    // Use config as the responsive configuration
+    .pipe(plugins.responsive(config, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 70,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Strip all metadata
+      withMetadata: false,
+      // don't create enlarged output images if images are already less than the required dimensions.
+      errorOnEnlargement: false,
+      skipOnEnlargement: true,
+      errorOnUnusedConfig: false
+    }))
+    .pipe(gulp.dest(dist.images));
 });
 
 // images task
 
-gulp.task('images', function () {
-    return gulp.src(paths.images, {
-            base: paths.base
-        })
-        .pipe(plugins.cached(plugins.imagemin({
-            optimizationLevel: 5,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest(dist.images))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('images', function() {
+  return gulp.src(paths.images, {
+      base: paths.base
+    })
+    .pipe(plugins.cached(plugins.imagemin({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest(dist.images))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // task to create optimised image assets for site
 // settings for site image generation
 
 var pngToJpgSizesConfig = [{
-    height: 75,
-    format: 'jpeg',
-    rename: {
-        suffix: '_xsmall',
-        extname: '.jpg'
-    }
-            }, {
-    height: 200,
-    format: 'jpeg',
-    rename: {
-        suffix: '_small',
-        extname: '.jpg'
-    }
-            }, {
-    height: 300,
-    format: 'jpeg',
-    rename: {
-        suffix: '_medium',
-        extname: '.jpg'
-    }
-            }, {
-    height: 450,
-    format: 'jpeg',
-    rename: {
-        suffix: '_large',
-        extname: '.jpg'
-    }
-            }, {
-    height: 750,
-    format: 'jpeg',
-    rename: {
-        suffix: '_xlarge',
-        extname: '.jpg'
-    }
-        }];
+  height: 75,
+  format: 'jpeg',
+  rename: {
+    suffix: '_xsmall',
+    extname: '.jpg'
+  }
+}, {
+  height: 200,
+  format: 'jpeg',
+  rename: {
+    suffix: '_small',
+    extname: '.jpg'
+  }
+}, {
+  height: 300,
+  format: 'jpeg',
+  rename: {
+    suffix: '_medium',
+    extname: '.jpg'
+  }
+}, {
+  height: 450,
+  format: 'jpeg',
+  rename: {
+    suffix: '_large',
+    extname: '.jpg'
+  }
+}, {
+  height: 750,
+  format: 'jpeg',
+  rename: {
+    suffix: '_xlarge',
+    extname: '.jpg'
+  }
+}];
 
 var imgSizesConfig = [{
-    height: 75,
-    rename: {
-        suffix: '_xsmall'
-    }
-            }, {
-    height: 200,
-    rename: {
-        suffix: '_small'
-    }
-            }, {
-    height: 300,
-    rename: {
-        suffix: '_medium'
-    }
-            }, {
-    height: 450,
-    rename: {
-        suffix: '_large'
-    }
-            }, {
-    height: 750,
-    rename: {
-        suffix: '_xlarge'
-    }
-        }];
+  height: 75,
+  rename: {
+    suffix: '_xsmall'
+  }
+}, {
+  height: 200,
+  rename: {
+    suffix: '_small'
+  }
+}, {
+  height: 300,
+  rename: {
+    suffix: '_medium'
+  }
+}, {
+  height: 450,
+  rename: {
+    suffix: '_large'
+  }
+}, {
+  height: 750,
+  rename: {
+    suffix: '_xlarge'
+  }
+}];
 
-gulp.task('imagesForSite', function () {
-    return gulp.src(paths.images, {
-            base: dist.base
-        })
-        .pipe(plugins.debug({
-            title: 'sources:'
-        }))
-        .pipe(plugins.cached('imagesForSite'))
-        .pipe(plugins.debug({
-            title: 'before gulp-responsive:'
-        }))
-        .pipe(plugins.responsive({
-            '!wAlpha/*.png': pngToJpgSizesConfig,
-            'wAlpha/*.png': imgSizesConfig,
-            '**/*.jpg': imgSizesConfig,
-        }, {
-            // Global configuration for all images
-            // The output quality for JPEG, WebP and TIFF output formats
-            quality: 80,
-            // Use progressive (interlace) scan for JPEG and PNG output
-            progressive: true,
-            // Zlib compression level of PNG output format
-            compressionLevel: 6,
-            // Strip all metadata
-            withMetadata: false,
-            errorOnEnlargement: false,
-            // only make sizes from xsmall to max size without enlarging the original src image
-            skipOnEnlargement: true,
-            // dont error - will passs through any non-matching image files
-            errorOnUnusedConfig: false
-        }))
-        .pipe(plugins.debug({
-            title: 'after responsive:'
-        }))
-        // make an assets directory in src and dist directories
-        .pipe(gulp.dest('src/assets'))
+gulp.task('imagesForSite', function() {
+  return gulp.src(paths.images, {
+      base: dist.base
+    })
+    .pipe(plugins.debug({
+      title: 'sources:'
+    }))
+    .pipe(plugins.cached('imagesForSite'))
+    .pipe(plugins.debug({
+      title: 'before gulp-responsive:'
+    }))
+    .pipe(plugins.responsive({
+      '!wAlpha/*.png': pngToJpgSizesConfig,
+      'wAlpha/*.png': imgSizesConfig,
+      '**/*.jpg': imgSizesConfig,
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 80,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Zlib compression level of PNG output format
+      compressionLevel: 6,
+      // Strip all metadata
+      withMetadata: false,
+      errorOnEnlargement: false,
+      // only make sizes from xsmall to max size without enlarging the original src image
+      skipOnEnlargement: true,
+      // dont error - will passs through any non-matching image files
+      errorOnUnusedConfig: false
+    }))
+    .pipe(plugins.debug({
+      title: 'after responsive:'
+    }))
+    // make an assets directory in src and dist directories
+    .pipe(gulp.dest('src/assets'));
 });
 
 // google page speed insights tasks
 
-gulp.task('mobile', function () {
-    return psi(site, {
-        // key: key
-        nokey: 'true',
-        strategy: 'mobile',
-    }).then(function (data) {
-        console.log('Speed score: ' + data.ruleGroups.SPEED.score);
-        console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
-    });
+gulp.task('mobile', function() {
+  return psi(site, {
+    // key: key
+    nokey: 'true',
+    strategy: 'mobile',
+  }).then(function(data) {
+    console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+    console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
+  });
 });
 
-gulp.task('desktop', function () {
-    return psi(site, {
-        //key: key,
-        nokey: 'true',
-        strategy: 'desktop',
-    }).then(function (data) {
-        console.log('Speed score: ' + data.ruleGroups.SPEED.score);
-    });
+gulp.task('desktop', function() {
+  return psi(site, {
+    //key: key,
+    nokey: 'true',
+    strategy: 'desktop',
+  }).then(function(data) {
+    console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+  });
 });
 
 // validate html task
 
-gulp.task('validateHtml', function () {
-    gulp.src(paths.content)
-        .pipe(plugins.w3cjs())
-        .pipe(through2.obj(function (file, enc, cb) {
-            cb(null, file);
-            if (!file.w3cjs.success) {
-                console.log(chalk.bgRed.bold('HTML validation error(s) found'));
-            }
-        }));
+gulp.task('validateHtml', function() {
+  gulp.src(paths.content)
+    .pipe(plugins.w3cjs())
+    .pipe(through2.obj(function(file, enc, cb) {
+      cb(null, file);
+      if (!file.w3cjs.success) {
+        console.log(chalk.bgRed.bold('HTML validation error(s) found'));
+      }
+    }));
 });
 
 // validate css task
 
-gulp.task('validateCss', function () {
-    gulp.src(paths.styles)
-        .pipe(plugins.cssValidator())
-        .on('error', function (err) {
-            console.log(chalk.bgRed.bold('CSS validation error(s) found'));
-        });
+gulp.task('validateCss', function() {
+  gulp.src(paths.styles)
+    .pipe(plugins.cssValidator())
+    .on('error', function(err) {
+      console.log(chalk.bgRed.bold('CSS validation error(s) found'));
+    });
 });
 
 // WATCH tasks
 
-gulp.task('watch-content', function () {
-    gulp.src(paths.content)
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
+gulp.task('watch-content', function() {
+  gulp.src(paths.content)
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-gulp.task('watch-styles', function () {
-    gulp.src(paths.styles)
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
+gulp.task('watch-styles', function() {
+  gulp.src(paths.styles)
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-gulp.task('watch-scripts', function () {
-    gulp.src(paths.scripts)
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
+gulp.task('watch-scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
 // Watch Files For Changes
-gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['watch-scripts']);
-    gulp.watch(paths.content, ['watch-content']);
-    gulp.watch(paths.styles, ['watch-styles']);
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['watch-scripts']);
+  gulp.watch(paths.content, ['watch-content']);
+  gulp.watch(paths.styles, ['watch-styles']);
 });
 
 
 // Lint Task
-gulp.task('lint', function () {
-    return gulp.src(paths.scripts, {
-            base: paths.base
-        })
-        .pipe(plugins.jshint('.jshintrc'))
-        .pipe(plugins.jshint.reporter('jshint-stylish'))
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts, {
+      base: paths.base
+    })
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
 
 // CLEAN Tasks
 
-gulp.task('build-clean', function () {
-    return del('dist/*');
+gulp.task('build-clean', function() {
+  return del('dist/*');
 });
 
-gulp.task('cleanCss', function () {
-    return gulp.src(paths.styles, {
-            base: paths.base
-        })
-        // TODO:  task using uncss causes errors
-        // strip out unused CSS rules for site
-        .pipe(plugins.uncss({
-            html: [paths.content]
-        }))
-        .pipe(gulp.dest(dist.styles))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('cleanCss', function() {
+  return gulp.src(paths.styles, {
+      base: paths.base
+    })
+    // TODO:  task using uncss causes errors
+    // strip out unused CSS rules for site
+    .pipe(plugins.uncss({
+      html: [paths.content]
+    }))
+    .pipe(gulp.dest(dist.styles))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // BUILD Tasks
 
-gulp.task('build-scripts', function () {
-    return gulp.src(paths.scripts, {
-            base: paths.base
-        })
-        .pipe(customPlumber('Error running scripts task'))
-        // Minify javascript files
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest(dist.scripts))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task('build-scripts', function() {
+  return gulp.src(paths.scripts, {
+      base: paths.base
+    })
+    .pipe(customPlumber('Error running scripts task'))
+    // Minify javascript files
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(dist.scripts))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('build-styles', function () {
-    return gulp.src(paths.styles, {
-            base: paths.base
-        })
-        // Minify css files
-        .pipe(plugins.cssnano())
-        .pipe(gulp.dest(dist.styles))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('build-styles', function() {
+  return gulp.src(paths.styles, {
+      base: paths.base
+    })
+    // Minify css files
+    .pipe(plugins.cssnano())
+    .pipe(gulp.dest(dist.styles))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('build-html', function () {
+gulp.task('build-html', function() {
 
-    return gulp.src(paths.content, {
-            base: paths.base
-        })
-        // inline any js & css resources flaged 'inline' in html file
-        .pipe(plugins.debug({
-            title: 'before:'
-        }))
-        // inline css file links using 'smoosher comments' in html file
-        .pipe(plugins.smoosher())
-        // minify html files
-        .pipe(plugins.htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true
-        }))
-        // output to dist directory
-        .pipe(gulp.dest(dist.content))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+  return gulp.src(paths.content, {
+      base: paths.base
+    })
+    // inline any js & css resources flaged 'inline' in html file
+    .pipe(plugins.debug({
+      title: 'before:'
+    }))
+    // inline css file links using 'smoosher comments' in html file
+    .pipe(plugins.smoosher())
+    // minify html files
+    .pipe(plugins.htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: true
+    }))
+    // output to dist directory
+    .pipe(gulp.dest(dist.content))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 
-gulp.task('build-assets', function () {
-    return gulp.src(paths.assets, {
-            base: paths.base
-        })
-        .pipe(gulp.dest(dist.assets))
+gulp.task('build-assets', function() {
+  return gulp.src(paths.assets, {
+      base: paths.base
+    })
+    .pipe(gulp.dest(dist.assets));
 });
 
 // lint JS, validate html & CSS task
@@ -410,11 +410,11 @@ gulp.task('validate', ['lint', 'validateHtml', 'validateCss']);
 
 // build task
 
-gulp.task('build', function (callback) {
-    runSequence('build-clean', ['build-scripts', 'build-styles'],
-        'build-html',
-        'build-assets',
-        callback);
+gulp.task('build', function(callback) {
+  runSequence('build-clean', ['build-scripts', 'build-styles'],
+    'build-html',
+    'build-assets',
+    callback);
 });
 // serve task
 
